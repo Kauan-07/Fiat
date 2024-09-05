99123cfb491692bd04554a30ca7c2bede86d0f35
Kauan-07 <160788373+Kauan-07@users.noreply.github.com>
2024-09-05 20:44:38 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+// Função de exemplo para manipulação de evento
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página carregada');
+    // Aqui você pode adicionar interatividade ou funcionalidades JS
+});
